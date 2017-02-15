0aba25c7ff7a4ea8a4c789c3f27c88780bb13147
Andrew Jack <andrew.jack@drivetribe.com>
2017-02-15 17:55:11 +0000

    Fix Example

--- android/src/main/java/com/inprogress/reactnativeyoutube/YouTubeView.java
+++ android/src/main/java/com/inprogress/reactnativeyoutube/YouTubeView.java
@@ -1,24 +1,22 @@
 package com.inprogress.reactnativeyoutube;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 
+@SuppressLint("ViewConstructor")
+public class YouTubeView extends FrameLayout {
 
-public class YouTubeView extends RelativeLayout {
-
-    YouTubePlayerController youtubeController;
-    private YouTubePlayerFragment youTubePlayerFragment;
-    public static String youtube_key;
+    YouTubePlayerController mYoutubeController;
+    private YouTubePlayerFragment mYouTubePlayerFragment;
 
     public YouTubeView(ReactContext context) {
         super(context);
@@ -26,38 +24,44 @@ public class YouTubeView extends RelativeLayout {
     }
 
     private ReactContext getReactContext() {
-        return (ReactContext)getContext();
+        return (ReactContext) getContext();
     }
 
     public void init() {
-        inflate(getContext(), R.layout.youtube_layout, this);
-        FragmentManager fragmentManager = getReactContext().getCurrentActivity().getFragmentManager();
-        youTubePlayerFragment = (YouTubePlayerFragment) fragmentManager
-                .findFragmentById(R.id.youtubeplayerfragment);
-        youtubeController = new YouTubePlayerController(YouTubeView.this);
+        ViewGroup.LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        setLayoutParams(layoutParams);
+        mYouTubePlayerFragment = YouTubePlayerFragment.newInstance();
+        mYoutubeController = new YouTubePlayerController(this);
     }
 
+    @Override
+    protected void onAttachedToWindow() {
+        Activity currentActivity = getReactContext().getCurrentActivity();
+        if (currentActivity != null) {
+            FragmentManager fragmentManager = getReactContext().getCurrentActivity().getFragmentManager();
+            fragmentManager.beginTransaction().add(getId(), mYouTubePlayerFragment).commit();
+        }
+    }
 
     @Override
     protected void onDetachedFromWindow() {
-        try {
-            FragmentManager fragmentManager = getReactContext().getCurrentActivity().getFragmentManager();
-            youTubePlayerFragment = (YouTubePlayerFragment) 
-                    fragmentManager.findFragmentById(R.id.youtubeplayerfragment);
-            FragmentTransaction ft = fragmentManager.beginTransaction();
-            ft.remove(youTubePlayerFragment);
-            ft.commit();
-        } catch (Exception e) {
+        Activity currentActivity = getReactContext().getCurrentActivity();
+        if (currentActivity != null) {
+            FragmentManager fragmentManager = currentActivity.getFragmentManager();
+            if (mYouTubePlayerFragment != null) {
+                fragmentManager.beginTransaction().remove(mYouTubePlayerFragment).commit();
+            }
         }
         super.onDetachedFromWindow();
     }
 
     public void seekTo(int second) {
-        youtubeController.seekTo(second);
+        mYoutubeController.seekTo(second);
     }
 
 
     public void playerViewDidBecomeReady() {
+        post(measureAndLayout);
         WritableMap event = Arguments.createMap();
         ReactContext reactContext = (ReactContext) getContext();
         event.putInt("target", getId());
@@ -103,47 +107,60 @@ public class YouTubeView extends RelativeLayout {
 
 
     public void setVideoId(String str) {
-        youtubeController.setVideoId(str);
+        mYoutubeController.setVideoId(str);
     }
 
     public void setInline(Boolean bool) {
-        youtubeController.setPlayInline(bool);
+        mYoutubeController.setPlayInline(bool);
     }
 
     public void setShowInfo(Boolean bool) {
-        youtubeController.setShowInfo(bool);
+        mYoutubeController.setShowInfo(bool);
     }
 
     public void setModestbranding(Boolean bool) {
-        youtubeController.setModestBranding(bool);
+        mYoutubeController.setModestBranding(bool);
     }
 
     public void setControls(Integer nb) {
-        youtubeController.setControls(nb);
+        mYoutubeController.setControls(nb);
     }
 
     public void setPlay(Boolean bool) {
-        youtubeController.setPlay(bool);
+        mYoutubeController.setPlay(bool);
     }
 
     public void setHidden(Boolean bool) {
-        youtubeController.setHidden(bool);
+        mYoutubeController.setHidden(bool);
     }
 
     public void setApiKey(String apiKey) {
-        youtube_key = apiKey;
-        youTubePlayerFragment.initialize(youtube_key, youtubeController);
+        try {
+            mYouTubePlayerFragment.initialize(apiKey, mYoutubeController);
+        } catch (Exception e) {
+            receivedError(e.getMessage());
+        }
     }
 
     public void setLoop(Boolean loop) {
-        youtubeController.setLoop(loop);
+        mYoutubeController.setLoop(loop);
     }
 
     public void setRelated(Boolean related) {
-        youtubeController.setRelated(related);
+        mYoutubeController.setRelated(related);
     }
 
     public void setFullscreen(Boolean bool) {
-        youtubeController.setFullscreen(bool);
+        mYoutubeController.setFullscreen(bool);
     }
+
+    private final Runnable measureAndLayout = new Runnable() {
+        @Override
+        public void run() {
+            measure(
+                    MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.EXACTLY),
+                    MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.EXACTLY));
+            layout(getLeft(), getTop(), getRight(), getBottom());
+        }
+    };
 }
